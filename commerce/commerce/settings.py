9abdcfdfe1fc3182829f757a90454726ed9555ea
thapakaazzi <nikesh.-73bct563@acem.edu.np>
2021-04-22 22:53:41 +0545

    static files 2.0

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -116,10 +116,15 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+ASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 LOGIN_URL = 'login'
 
+# Extra places for collectstatic to find static files.
 
